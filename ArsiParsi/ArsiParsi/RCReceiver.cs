14d9c01fe18cb00ba6a07534a3b0a7b80b3fff52
Brc <dbrckovi@gmail.com>
2022-12-29 13:39:12 +0100

    Code readability

--- ArsiParsi/ArsiParsi/RCReceiver.cs
+++ ArsiParsi/ArsiParsi/RCReceiver.cs
@@ -13,7 +13,7 @@ namespace ArsiParsi
     public const int DATA_RECEIVE_DELAY_MS = 10;  //number of milliseconds to wait for new data after the last data was received, before processing it
 
     SerialPort _port;
-    StringBuilder _dataBuilder = new StringBuilder();
+    StringBuilder _dataBuffer = new StringBuilder();
     DateTime _lastRawDataReceiveTime = DateTime.Now;
     Thread _worker = null;
 
@@ -64,31 +64,19 @@ namespace ArsiParsi
       }
     }
 
+    /// <summary>
+    /// Compiles and re-formats the raw data when it was received and enough time has passed so we can be fairly sure the message is complete
+    /// </summary>
     private void Work()
     {
       while(_port != null)
       {
-        if (_dataBuilder.Length > 0)
+        if (_dataBuffer.Length > 0)
         {
           TimeSpan span = DateTime.Now - _lastRawDataReceiveTime;
           if (span.TotalMilliseconds > DATA_RECEIVE_DELAY_MS)
           {
-            string data = _dataBuilder.ToString();
-            _dataBuilder.Clear();
-
-            if (RemoveEmptyLines && !string.IsNullOrEmpty(data))
-            {
-              string[] lines = data.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-              StringBuilder sb = new StringBuilder();
-
-              foreach (string line in lines)
-              {
-                sb.AppendLine(line);
-              }
-              data = sb.ToString();
-            }
-
-            RawDataReceived?.Invoke(data);
+            DispatchCompletedData();
           }
         }
         
@@ -96,18 +84,35 @@ namespace ArsiParsi
       }
     }
 
+    /// <summary>
+    /// Compiles and re-formats the raw data, clears the buffer and dispatches it to further processing
+    /// </summary>
+    private void DispatchCompletedData()
+    {
+      string data = _dataBuffer.ToString();
+      _dataBuffer.Clear();
+
+      if (RemoveEmptyLines && !string.IsNullOrEmpty(data))
+      {
+        string[] lines = data.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+        StringBuilder sb = new StringBuilder();
+
+        foreach (string line in lines)
+        {
+          sb.AppendLine(line);
+        }
+        data = sb.ToString();
+      }
+
+      RawDataReceived?.Invoke(data);
+    }
+
     private void _port_DataReceived(object sender, SerialDataReceivedEventArgs e)
     {
+      //Just append the data to the buffer and log the time. Worker will handle the rest.
       string data = _port.ReadExisting();
-      _dataBuilder.Append(data);
+      _dataBuffer.Append(data);
       _lastRawDataReceiveTime = DateTime.Now;
-
-
-
-      _lastRawDataReceiveTime = DateTime.Now;
-
-
-      //RawDataReceived?.Invoke(data);
     }
   }
 }
